8029ba74d99f9438e9072f00a6ca70589e985f25
Vaibhav Pandey <fcvaibhav@gmail.com>
2013-07-13 19:57:10 +0000

    2nd modification to web.js

--- web.js
+++ web.js
@@ -1,26 +1,24 @@
-var fs = require('fs');
-
-var Buffer = require('Buffer')
-
-var buffernew = new Buffer(40);
-
-var buffernew;
-fs.readFileSync('index.html', function(err, data) {
-if (err) {
-throw wrr;
-}
-buffernew = data;
-});
-
-var message = buffernew.tostring("utf-8",0,buffernew.length);
+var fs = require('fs')
 
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffernew = new Buffer(40);
+
+var buffernew;
+// First I want to read the file
+fs.readFile('index.html', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    buffernew = data;
+    var content = buffernew.toString("utf-8", 0, buffernew.length)
 app.get('/', function(request, response) {
-  response.send(message);
+  response.send(content);
 });
+
+    });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
